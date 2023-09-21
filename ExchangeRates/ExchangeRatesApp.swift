932f4ed40e9817dbaaf0393a4e0851694f9f4d1c
Rafael Badaro <rafabadaro.15@gmail.com>
2023-09-21 00:08:38 -0300

    Initial Commit

---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Rafael Badaró on 21/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
